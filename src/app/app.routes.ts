8675a6b8657996e010f7b3137e9e75b8f6e406ab
Ahmed Zienhom <ahmedalnafy@gmail.com>
2025-05-04 02:07:26 +0300

    some fixes and features

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,8 @@ import { confirmGuard } from './core/guards/confirm.guard';
 
 
 export const routes: Routes = [
-    {path:"", redirectTo:"crud",pathMatch:'full'},
-    {path:"crud", component:ManageComponent,title:"CRUD"},
+    {path:"", redirectTo:"",pathMatch:'full'},
+    {path:"", component:ManageComponent,title:"CRUD"},
     {path:"addEmployee",loadComponent:() => import('./components/add/add.component').then(c => c.AddComponent),title:"Add New Employee" ,canDeactivate:[confirmGuard]},
     {path:'edit/:id',loadComponent:() => import('./components/edit/edit.component').then(c => c.EditComponent),title:"Edit",canDeactivate:[confirmGuard]},
     {path:"**",loadComponent:() => import('./components/not-found/not-found.component').then(c => c.NotFoundComponent),title:"Not Found"}
